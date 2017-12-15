3e36191d6d1467369ab9f67a71e210d5e296eba5
Brody Macfarlane <macfarlanebrody@gmail.com>
2017-12-15 12:38:48 -0700

    css and landing page

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import maroonLogo from '../assets/maroon-logo.svg'
 
 export default class Login extends Component {
     render() {
         return(
             <div className="component login-component">
-                <div className="main-container login-container"></div>
+                <div className="main-container">
+                    <div className="content-container">
+                        <img className="maroon-logo" src={maroonLogo}/>
+                        <div className="login-title">BOOK EXCHANGE</div>
+                        <div id="credentials-container">
+                            <div className="credentials">
+                                <div>Username</div>
+                                <input type="text"/>
+                            </div>
+                            <div className="credentials">
+                                <div>Password</div>
+                                <input type="password"/>
+                            </div>
+                            <div className="credentials-btn">
+                                <button>REGISTER</button>
+                                <a href="/#/browse">
+                                    <button>LOGIN</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
